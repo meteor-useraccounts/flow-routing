2272e6df30a710b251df2696a880b1653ffd5efb
Luca Mussi <ing.luca.mussi@gmail.com>
2015-10-05 22:43:28 +0200

    linting

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
     Accounts: false,
     BlazeLayout: false,
     FlowRouter: false,
+    Match: false,
     UAFlowRouting: false,
     UALog: false,
     UserAccounts: false
@@ -14,36 +15,38 @@
 // ------------------------------------------
 UALog.trace('Loading main.js');
 
-
 // Plugin configuration pattern to be checked with check
 var CONFIG_PAT = {
-  defaultTemplate: Match.Optional(String),
-  defaultLayout: Match.Optional(String),
-  defaultLayoutRegions: Match.Optional(Object),
-  defaultContentRegion: Match.Optional(String),
+  defaultTemplate: new Match.Optional(String),
+  defaultLayout: new Match.Optional(String),
+  defaultLayoutRegions: new Match.Optional(Object),
+  defaultContentRegion: new Match.Optional(String),
 };
 
 // Route configuration pattern to be checked with check
 var ROUTE_PAT = {
-  contentRegion: Match.Optional(String),
-  layoutRegions: Match.Optional(String),
-  layoutTemplate: Match.Optional(String),
-  name: Match.Optional(String),
-  path: Match.Optional(String),
-  redirect: Match.Optional(Match.OneOf(String, Match.Where(_.isFunction))),
-  template: Match.Optional(String),
+  contentRegion: new Match.Optional(String),
+  layoutRegions: new Match.Optional(String),
+  layoutTemplate: new Match.Optional(String),
+  name: new Match.Optional(String),
+  path: new Match.Optional(String),
+  redirect: new Match.Optional( new Match.OneOf(
+    String,
+    new Match.Where(_.isFunction)
+  )),
+  template: new Match.Optional(String),
 };
 
 var ROUTE_DEFAULT = {
-  changePwd:       { name: "uaChangePwd",       path: "/change-password"},
-  enrollAccount:   { name: "uaEnrollAccount",   path: "/enroll-account"},
-  ensureSignedIn:  { name: "uaEnsureSignedIn",  path: null},
-  forgotPwd:       { name: "uaForgotPwd",       path: "/forgot-password"},
-  resetPwd:        { name: "uaResetPwd",        path: "/reset-password"},
-  signIn:          { name: "uaSignIn",          path: "/sign-in"},
-  signUp:          { name: "uaSignUp",          path: "/sign-up"},
-  verifyEmail:     { name: "uaVerifyEmail",     path: "/verify-email"},
-  sendVerifyEmail: { name: "uaSendVerifyEmail", path: "/send-verify-email"},
+  changePwd: { name: 'uaChangePwd', path: '/change-password'},
+  enrollAccount: { name: 'uaEnrollAccount', path: '/enroll-account'},
+  ensureSignedIn: { name: 'uaEnsureSignedIn', path: null},
+  forgotPwd: { name: 'uaForgotPwd', path: '/forgot-password'},
+  resetPwd: { name: 'uaResetPwd', path: '/reset-password'},
+  signIn: { name: 'uaSignIn', path: '/sign-in'},
+  signUp: { name: 'uaSignUp', path: '/sign-up'},
+  verifyEmail: { name: 'uaVerifyEmail', path: '/verify-email'},
+  sendVerifyEmail: { name: 'uaSendVerifyEmail', path: '/send-verify-email'},
 };
 
 
@@ -57,7 +60,7 @@ _.extend(UAFlowRouting, {
   defaultContentRegion: null,
 
   // Redirects
-  homeRoutePath: "/",
+  homeRoutePath: '/',
   redirectTimeout: 2000, // 2 seconds
 
   // Known routes used to filter out previous path for redirects...
@@ -66,7 +69,7 @@ _.extend(UAFlowRouting, {
   // Configured routes
   routes: {},
 
-  configure: function(options) {
+  configure: function configure(options) {
     check(options, CONFIG_PAT);
     UALog.trace('configure ' + this._id);
     // console.log(options);
@@ -84,23 +87,26 @@ _.extend(UAFlowRouting, {
       options.defaultTemplate || this.defaultTemplate;
   },
 
-  configureRoute: function(route, options) {
+  configureRoute: function configureRoute(route, options) {
     check(route, String);
-    check(options, Match.OneOf(undefined, Match.ObjectIncluding(ROUTE_PAT)));
+    check(options, new Match.OneOf(
+      undefined,
+      new Match.ObjectIncluding(ROUTE_PAT))
+    );
     options = _.clone(options);
 
     // Only allowed routes can be configured
     if (!(route in ROUTE_DEFAULT)) {
-      throw new Error("Unknown Route!");
+      throw new Error('Unknown Route!');
     }
     // Allow route configuration only once
     if (route in this.routes) {
-      throw new Error("Route already configured!");
+      throw new Error('Route already configured!');
     }
 
     // Possibly adds a initial / to the provided path
-    if (options && options.path && options.path[0] !== "/") {
-      options.path = "/" + options.path;
+    if (options && options.path && options.path[0] !== '/') {
+      options.path = '/' + options.path;
     }
 
     // Updates the current configuration
@@ -113,55 +119,55 @@ _.extend(UAFlowRouting, {
     this.knownRoutes.push(options.name);
 
     if (Meteor.isServer) {
-      // Configures "reset password" email link
-      if (route === "resetPwd") {
+      // Configures 'reset password' email link
+      if (route === 'resetPwd') {
         var resetPwdPath = options.path.substr(1);
-        Accounts.urls.resetPassword = function(token) {
-          return Meteor.absoluteUrl(resetPwdPath + "/" + token);
+        Accounts.urls.resetPassword = function resetPassword(token) {
+          return Meteor.absoluteUrl(resetPwdPath + '/' + token);
         };
       }
-      // Configures "enroll account" email link
-      if (route === "enrollAccount") {
+      // Configures 'enroll account' email link
+      if (route === 'enrollAccount') {
         var enrollAccountPath = options.path.substr(1);
-        Accounts.urls.enrollAccount = function(token) {
-          return Meteor.absoluteUrl(enrollAccountPath + "/" + token);
+        Accounts.urls.enrollAccount = function enrollAccount(token) {
+          return Meteor.absoluteUrl(enrollAccountPath + '/' + token);
         };
       }
-      // Configures "verify email" email link
-      if (route === "verifyEmail") {
+      // Configures 'verify email' email link
+      if (route === 'verifyEmail') {
         var verifyEmailPath = options.path.substr(1);
-        Accounts.urls.verifyEmail = function(token) {
-          return Meteor.absoluteUrl(verifyEmailPath + "/" + token);
+        Accounts.urls.verifyEmail = function verifyEmail(token) {
+          return Meteor.absoluteUrl(verifyEmailPath + '/' + token);
         };
       }
     }
 
-    if (route === "ensureSignedIn") {
+    if (route === 'ensureSignedIn') {
       return;
     }
 
     /*
-    if (route === "changePwd" && !UserAccounts.options.enablePasswordChange) {
+    if (route === 'changePwd' && !UserAccounts.options.enablePasswordChange) {
       throw new Error(
-        "changePwd route configured but enablePasswordChange set to false!"
+        'changePwd route configured but enablePasswordChange set to false!'
       );
     }
-    if (route === "forgotPwd" && !UserAccounts.options.showForgotPasswordLink) {
+    if (route === 'forgotPwd' && !UserAccounts.options.showForgotPasswordLink) {
       throw new Error(
-        "forgotPwd route configured but showForgotPasswordLink set to false!"
+        'forgotPwd route configured but showForgotPasswordLink set to false!'
       );
     }
-    if (route === "signUp"
+    if (route === 'signUp'
       && UserAccounts.options.forbidClientAccountCreation) {
       throw new Error(
-        "signUp route configured but forbidClientAccountCreation set to true!"
+        'signUp route configured but forbidClientAccountCreation set to true!'
       );
     }
     */
 
     // fullPageAtForm template unless user specified a different
     // site-wide default
-    var defaultTemplate = this.defaultTemplate || "uaFullPageForm";
+    var defaultTemplate = this.defaultTemplate || 'uaFullPageForm';
     // Determines the default layout to be used in case no specific one is
     // specified for single routes
     var defaultLayout = this.defaultLayout;
@@ -179,54 +185,54 @@ _.extend(UAFlowRouting, {
     layoutRegions[contentRegion] = template;
 
     // Possibly adds token parameter
-    if (_.contains(["enrollAccount", "resetPwd", "verifyEmail"], route)) {
-      path += "/:paramToken";
-      if (route === "verifyEmail") {
+    if (_.contains(['enrollAccount', 'resetPwd', 'verifyEmail'], route)) {
+      path += '/:paramToken';
+      if (route === 'verifyEmail') {
         FlowRouter.route(path, {
           name: name,
           triggersEnter: [
-            function() {
+            function triggersEnter() {
               UserAccounts.setInitialState(route);
               /*
               UserAccounts.setDisabled(true);
               */
-            }
+            },
           ],
-          action: function(params) {
+          action: function action(params) {
             BlazeLayout.render(layoutTemplate, layoutRegions);
 
             var token = params.paramToken;
-            Accounts.verifyEmail(token, function(error) {
+            Accounts.verifyEmail(token, function verifyEmail(error) {
               // TODO: ...
               /*
               UserAccounts.setDisabled(false);
               UserAccounts.submitCallback(error, route, function() {
                 UserAccounts.state.form.set(
-                  "result", UserAccounts.texts.info.emailVerified
+                  'result', UserAccounts.texts.info.emailVerified
                 );
               });
               */
             });
-          }
+          },
         });
       } else {
         FlowRouter.route(path, {
           name: name,
           triggersEnter: [
-            function() {
+            function triggersEnter() {
               UserAccounts.setInitialState(route);
-            }
+            },
           ],
-          action: function() {
+          action: function action() {
             BlazeLayout.render(layoutTemplate, layoutRegions);
-          }
+          },
         });
       }
     } else {
       FlowRouter.route(path, {
         name: name,
         triggersEnter: [
-          function() {
+          function triggersEnter() {
             var redirect = false;
             if (route === 'changePwd') {
               if (!Meteor.loggingIn() && !Meteor.userId()) {
@@ -242,29 +248,29 @@ _.extend(UAFlowRouting, {
             } else {
               UserAccounts.setInitialState(route);
             }
-          }
+          },
         ],
-        action: function() {
+        action: function action() {
           BlazeLayout.render(layoutTemplate, layoutRegions);
-        }
+        },
       });
     }
   },
 
 
-  getRouteName: function(route) {
+  getRouteName: function getRouteName(route) {
     if (route in this.routes) {
       return this.routes[route].name;
     }
     return null;
   },
 
-  getRoutePath: function(route) {
+  getRoutePath: function getRoutePath(route) {
     if (route in this.routes) {
       return this.routes[route].path;
     }
-    return "#";
-  }
+    return '#';
+  },
 
 });
 
