a5150f72222c64aa35d6565fabf249cad2ec31a7
Luca Mussi <ing.luca.mussi@gmail.com>
2015-10-05 22:41:41 +0200

    udpates and cleanup

--- package.js
+++ package.js
@@ -15,36 +15,36 @@ Package.describe({
   name: 'useraccounts:flow-routing',
   summary: 'UserAccounts package providing routes configuration capability via kadira:flow-router.',
   version: '2.0.0',
-  git: 'https://github.com/meteor-useraccounts/flow-routing.git'
+  git: 'https://github.com/meteor-useraccounts/flow-routing.git',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function pkgOnUse(api) {
   api.versionsFrom('1.0');
 
   // Logger
   api.use([
     'check',
     'jag:pince@0.0.9',
+    'kadira:blaze-layout',
+    'kadira:flow-router',
+    'reactive-var',
     'underscore',
     'useraccounts:core',
-    'kadira:flow-router',
-    'kadira:blaze-layout',
-    'reactive-var',
   ], Both);
 
   api.imply([
+    'kadira:blaze-layout@2.1.0',
+    'kadira:flow-router@2.6.2',
     'useraccounts:core@2.0.0',
-    'kadira:flow-router@2.0.1',
-    'kadira:blaze-layout@2.0.0',
   ], Both);
 
   api.addFiles([
     'src/_globals.js',
     'src/logger.js',
-    'src/main.js'
+    'src/main.js',
   ], Both);
 
   api.addFiles([
-    'src/client.js'
+    'src/client.js',
   ], Client);
 });
